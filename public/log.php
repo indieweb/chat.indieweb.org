d9ef69b2485209a139126d87e8dc4b9de5e6521b
Aaron Parecki <aaron@parecki.com>
2016-07-11 01:49:52 +0000

    add slack hook

--- public/log.php
+++ public/log.php
@@ -20,11 +20,40 @@ loadUsers();
 $tz = new DateTimeZone('UTC');
 $html = format_line($params['channel']['name'], $date, $tz, json_decode($input));
 
-$ch = curl_init('https://chat.indieweb.org/__/pub?id=chat');
+// Publish to the realtime logs
+$ch = curl_init(Config::$base.'__/pub?id=chat');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 $data = [];
 $data['html'] = $html;
 $data['channel'] = $params['channel']['name'];
+$data['type'] = $params['type'];
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 curl_exec($ch);
+
+
+// If a Slack channel is configured for this IRC channel, post it there too
+if($params['author']['username'] != '~slackuser' && ($channel=Config::slack_channel_for_irc_channel($params['channel']['name']))) {
+
+  $msg = Config::slack_message_replacements($params['content']);
+  $user = userForNick($params['author']['nickname']);
+  $icon = '';
+  if($user && property_exists($user->properties, 'photo')) {
+    $icon = ImageProxy::url($user->properties->photo[0]);
+  }
+  
+  $payload = array(
+    'text' => $msg,
+    'username' => $params['author']['nickname'],
+    'icon_url' => $icon,
+    'channel' => $channel,
+  );
+  
+  $ch = curl_init(Config::$slack_hook);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('payload'=>json_encode($payload))));
+  curl_exec($ch);
+  
+}
+  
