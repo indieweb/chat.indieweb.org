31814fb8681723cfc46b57bdcf4ee16622021b8e
Aaron Parecki <aaron@parecki.com>
2021-07-01 14:10:02 +0000

    remove log->slack hook, add temporary spam check

--- public/log.php
+++ public/log.php
@@ -10,12 +10,28 @@ unset($params['response_url']);
 
 $date = DateTime::createFromFormat('U.u', $params['timestamp']);
 
+/*
+// Temporary spam check
+// Check here to see if this is spam
+$ch = curl_init('http://api.loqi.me/is_spam.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $input);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+$check = json_decode(curl_exec($ch), true);
+if($check['spam'] == true) {
+  header('HTTP/1.1 403 Spam');
+  die();
+}
+*/
+
 
 if($params['channel']['name'] !== '#indieweb-chat') {
   $db = new Quartz\DB('data/'.$params['server'].'/'.$params['channel']['name'], 'w');
   $db->add($date, $params);
   $db->close();
 }
+
+redis()->incr('indieweb-chat-total-messages');
 
 $local_channel = Config::irc_channel_to_local_channel($params['channel']['name']);
 
@@ -39,35 +55,13 @@ if($params['channel']['name'] !== '#indieweb-chat') {
 
 }
 
-// If a Slack channel is configured for this IRC channel, post it there too
-if($params['author']['username'] != '~slackuser' && ($channel=Config::slack_channel_for_irc_channel($params['channel']['name']))) {
-
-  $msg = Config::slack_message_replacements($params['content']);
-  $user = userForNick($params['author']['nickname']);
-  $icon = '';
-  if($user && property_exists($user->properties, 'photo')) {
-    $icon = ImageProxy::url($user->properties->photo[0]);
-  }
-  
-  $payload = array(
-    'text' => $msg,
-    'username' => $params['author']['nickname'],
-    'icon_url' => $icon,
-    'channel' => $channel,
-  );
-  
-  $ch = curl_init(Config::$slack_hook);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('payload'=>json_encode($payload))));
-  curl_exec($ch);
-  
-}
 
 if($params['channel']['name'] != '#indieweb-chat') {
   // Notify the WebSub hub that there is new content
   $url = Config::base_url_for_channel($params['channel']['name']);
   if(!mc()->get('websub-throttle:'.$url)) {
+
+    $debug = date('c')."\nattempting to deliver PuSH";
 
     $ch = curl_init(Config::$hub);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -76,7 +70,10 @@ if($params['channel']['name'] != '#indieweb-chat') {
       'hub.mode' => 'publish',
       'hub.url' => $url
     ]));
-    curl_exec($ch);
+    $response = curl_exec($ch);
+
+    $debug .= "\n\n".$response."\n";
+    file_put_contents('/web/sites/chat.indieweb.org/maintenance/logs/push.txt', $debug);
 
     // throttle notifications to 2 minutes
     mc()->set('websub-throttle:'.$url, 1, 120);
