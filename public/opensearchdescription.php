ba4ad9383c77d6826da63172a37a9d84323cb025
Aaron Parecki <aaron@parecki.com>
2024-10-22 19:16:30 -0700

    Create opensearchdescription.php

---
+++ public/opensearchdescription.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+ <ShortName>search chat.indieweb.org</ShortName>
+ <Description>Search</Description>
+ <Url type="application/atom+xml" rel="results"
+      template="https://indiechat.search.cweiske.de/?q={searchTerms}&amp;page={startPage?}&amp;format=opensearch"/>
+ <Url type="text/html" rel="results" method="get"
+      template="https://indiechat.search.cweiske.de/?q={searchTerms}"/>
+</OpenSearchDescription>
